f23e4de9ab6ec7f2ca3a0454024d0ac4d58917a8
Nishant Mishra <nishant.28@outlook.com>
2025-10-11 10:54:15 +0530

    refactor: make code more modular
    This will help in making the UI responsive in further commits

--- composeApp/src/wasmJsMain/kotlin/org/nsh07/nsh07/ui/homeScreen/AppHomeScreen.kt
+++ composeApp/src/wasmJsMain/kotlin/org/nsh07/nsh07/ui/homeScreen/AppHomeScreen.kt
@@ -1,26 +1,18 @@
 package org.nsh07.nsh07.ui.homeScreen
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
-import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.text.*
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
-import nsh07.composeapp.generated.resources.*
-import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -76,24 +68,7 @@ fun AppHomeScreen(
     ) {
         Column(Modifier.padding(vertical = 96.dp).weight(1f)) {
             Column(Modifier.verticalScroll(rememberScrollState())) {
-                Text(
-                    "Nishant Mishra",
-                    style = typography.displayLarge.copy(fontSize = 48.sp),
-                    color = colorScheme.onSurface
-                )
-                Spacer(Modifier.height(4.dp))
-                Text(
-                    "App developer",
-                    style = typography.titleLarge,
-                    color = colorScheme.onSurface
-                )
-                Spacer(Modifier.height(20.dp))
-                Text(
-                    "I build performant, beautiful apps for mobile and desktop.",
-                    style = typography.bodyLarge,
-                    color = colorScheme.onSurfaceVariant,
-                    modifier = Modifier.widthIn(max = 320.dp)
-                )
+                NameAndDesc()
 
                 Spacer(Modifier.height(72.dp))
 
@@ -127,36 +102,7 @@ fun AppHomeScreen(
 
             Spacer(Modifier.weight(1f))
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                IconButton(onClick = { uriHandler.openUri("https://github.com/nsh07") }) {
-                    Icon(
-                        painterResource(Res.drawable.github),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-                IconButton(onClick = { uriHandler.openUri("https://gitlab.com/nsh07") }) {
-                    Icon(
-                        painterResource(Res.drawable.gitlab),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-                IconButton(onClick = { uriHandler.openUri("https://www.linkedin.com/in/nsh07/") }) {
-                    Icon(
-                        painterResource(Res.drawable.linkedin),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-                IconButton(onClick = { uriHandler.openUri("mailto:nishant.28@outlook.com") }) {
-                    Icon(
-                        painterResource(Res.drawable.email),
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-            }
+            SocialIcons()
         }
 
         LazyColumn(
@@ -164,155 +110,13 @@ fun AppHomeScreen(
             contentPadding = PaddingValues(vertical = 96.dp),
             modifier = Modifier.fillMaxHeight().weight(1.1f)
         ) {
-            items(paragraphs, key = { it.substring(0, 16) }) {
-                Text(
-                    it,
-                    style = typography.bodyLarge,
-                    color = colorScheme.onSurfaceVariant,
-                    modifier = Modifier.padding(start = cardPadding, bottom = 16.dp, end = cardPadding)
-                )
-            }
-            item("experience spacer") { Spacer(Modifier.height(112.dp)) }
-            items(experiences, key = { it.start }) {
-                ExperienceCard(experience = it, cardPadding = cardPadding, modifier = Modifier.padding(bottom = 32.dp))
-            }
-            item("linkedin link text") {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .padding(start = cardPadding)
-                        .clickable { uriHandler.openUri("https://www.linkedin.com/in/nsh07/") }
-                ) {
-                    Text("View LinkedIn profile ", style = typography.bodyLarge, fontWeight = FontWeight.SemiBold)
-                    Icon(painterResource(Res.drawable.open_in_browser), null, Modifier.size(16.dp))
-                }
-                Spacer(Modifier.height(112.dp))
-            }
-            if (!projectState.isLoading)
-                items(projectState.projects, key = { it.id }) {
-                    ProjectCard(
-                        project = it,
-                        cardPadding = cardPadding,
-                        modifier = Modifier.padding(bottom = 32.dp)
-                    )
-                }
-            else item {
-                LinearWavyProgressIndicator(
-                    Modifier.fillMaxWidth().padding(cardPadding).padding(bottom = 32.dp)
-                )
-            }
-            item("github link text") {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .padding(start = cardPadding)
-                        .clickable { uriHandler.openUri("https://github.com/nsh07") }
-                ) {
-                    Text("View all projects on GitHub ", style = typography.bodyLarge, fontWeight = FontWeight.SemiBold)
-                    Icon(painterResource(Res.drawable.open_in_browser), null, Modifier.size(16.dp))
-                }
-                Spacer(Modifier.height(112.dp))
-            }
-            item("work in progress") {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxWidth().padding(vertical = 56.dp)
-                ) {
-                    Icon(
-                        painterResource(Res.drawable.forklift),
-                        null,
-                        modifier = Modifier.size(64.dp)
-                    )
-                    Spacer(Modifier.height(16.dp))
-                    Text("Work in progress", style = typography.headlineSmall)
-                }
-            }
-            item("tech stack spacer") { Spacer(Modifier.height(112.dp)) }
-            item("tech stack") {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(horizontal = cardPadding)
-                ) {
-                    Text(
-                        buildAnnotatedString {
-                            append("Built with ")
-                            withLink(
-                                LinkAnnotation.Url(
-                                    url = "https://www.jetbrains.com/compose-multiplatform/",
-                                    styles = TextLinkStyles(SpanStyle(color = colorScheme.onSurface))
-                                )
-                            ) {
-                                append("Compose Multiplatform ")
-                            }
-                        },
-                        style = typography.bodyMedium,
-                        color = colorScheme.outline
-                    )
-                    Icon(
-                        painterResource(Res.drawable.compose),
-                        null,
-                        modifier = Modifier.clickable { uriHandler.openUri("https://www.jetbrains.com/compose-multiplatform/") }
-                    )
-                    Text(" in Kotlin. Deployed with ", style = typography.bodyMedium, color = colorScheme.outline)
-                    Icon(
-                        painterResource(Res.drawable.githubpages),
-                        null,
-                        modifier = Modifier
-                            .height(20.dp)
-                            .clickable { uriHandler.openUri("https://pages.github.com/") }
-                    )
-                    Text(".", style = typography.bodyMedium, color = colorScheme.outline)
-                }
-                Text(
-                    buildAnnotatedString {
-                        append("Text is set in ")
-                        withLink(
-                            LinkAnnotation.Url(
-                                url = "https://fonts.google.com/specimen/DM+Serif+Text",
-                                styles = TextLinkStyles(
-                                    SpanStyle(
-                                        color = colorScheme.onSurface,
-                                        fontFamily = typography.displayLarge.fontFamily
-                                    )
-                                )
-                            )
-                        ) {
-                            append("DM Serif Text")
-                        }
-                        append(" and ")
-                        withLink(
-                            LinkAnnotation.Url(
-                                url = "https://rsms.me/inter/",
-                                styles = TextLinkStyles(SpanStyle(color = colorScheme.onSurface))
-                            )
-                        ) {
-                            append("Inter")
-                        }
-                        append('.')
-                    },
-                    color = colorScheme.outline,
-                    style = typography.bodyMedium,
-                    modifier = Modifier.padding(horizontal = cardPadding)
-                )
-                Text(
-                    buildAnnotatedString {
-                        append("Layout inspired by ")
-                        withLink(
-                            LinkAnnotation.Url(
-                                url = "https://brittanychiang.com/",
-                                styles = TextLinkStyles(SpanStyle(color = colorScheme.onSurface))
-                            )
-                        ) {
-                            append("Brittany Chiang")
-                        }
-                        append("'s website.")
-                    },
-                    color = colorScheme.outline,
-                    style = typography.bodyMedium,
-                    modifier = Modifier.padding(horizontal = cardPadding)
-                )
-            }
+            mainContent(
+                paragraphs,
+                experiences,
+                projectState,
+                cardPadding,
+                uriHandler
+            )
         }
     }
 }
